fe3767561e76b10fbfb789982f6232404e4f3d27
shakeelriyaj <130513037+shakeelriyaj@users.noreply.github.com>
2023-09-15 19:04:51 -0700

    Adding benji and oliver card (#1)
    * added oliver
    
    * added emails

--- src/MemberCard.js
+++ src/MemberCard.js
@@ -15,9 +15,31 @@ import "./styles/members.css"
 import Natanael from "./assets/Natanael.jpg"
 import Ethan from "./assets/Ethanvu.jpeg"
 import Shakeel from "./assets/Shakeel.jpg"
+import Oliver from "./assets/Oliver.jpg"
+import { useState } from 'react';
 
 export default function BioCard() {
-  return (
+
+    const [showOliver, setShowOliver] = useState(false);
+    const [showEthan, setShowEthan] = useState(false);
+    const [showShakeel, setShowShakeel] = useState(false);
+    const [showNatanael, setShowNatanael] = useState(false);
+
+    const toggleOliverEmail = () => {
+        setShowOliver(!showOliver);
+    };
+    const toggleEthanEmail = () => {
+        setShowEthan(!showEthan);
+    };
+    const toggleNatanaelEmail = () => {
+        setShowNatanael(!showNatanael);
+    };
+    const toggleShakeelEmail = () => {
+        setShowShakeel(!showShakeel);
+    };
+
+
+    return (
     <div className="ParentBox">
         <Card
         sx={{
@@ -74,8 +96,9 @@ export default function BioCard() {
         <CardOverflow sx={{ bgcolor: 'background.level1' }}>
             <CardActions buttonFlex="1">
             <ButtonGroup variant="outlined" sx={{ bgcolor: 'background.surface' }}>
-                <Button>Message</Button>
+                <Button onClick={toggleNatanaelEmail}>Message</Button>
             </ButtonGroup>
+            {showNatanael && <div>nalpay@uci.edu</div>}
             </CardActions>
         </CardOverflow>
         </Card>
@@ -135,8 +158,9 @@ export default function BioCard() {
         <CardOverflow sx={{ bgcolor: 'background.level1' }}>
             <CardActions buttonFlex="1">
             <ButtonGroup variant="outlined" sx={{ bgcolor: 'background.surface' }}>
-                <Button>Message</Button>
+                <Button onClick={toggleShakeelEmail}>Message</Button>
             </ButtonGroup>
+            {showShakeel && <div>sriyaj@uci.edu</div>}
             </CardActions>
         </CardOverflow>
         </Card>
@@ -196,8 +220,71 @@ export default function BioCard() {
         <CardOverflow sx={{ bgcolor: 'background.level1' }}>
             <CardActions buttonFlex="1">
             <ButtonGroup variant="outlined" sx={{ bgcolor: 'background.surface' }}>
-                <Button>Message</Button>
+                <Button onClick={toggleEthanEmail}>Message</Button>
             </ButtonGroup>
+            {showEthan && <div>ethancv@uci.edu</div>}
+            </CardActions>
+        </CardOverflow>
+        </Card>
+{/***************************************************************/}
+        <Card
+        sx={{
+            width: 300,
+            maxWidth: '100%',
+            boxShadow: 'lg',
+        }}
+        >
+        <CardContent sx={{ alignItems: 'center', textAlign: 'center' }}>
+            <Avatar src={Oliver} sx={{ '--Avatar-size': '7rem' }} />
+            <Chip
+            size="sm"
+            variant="soft"
+            color="primary"
+            sx={{
+                mt: -1,
+                mb: 1,
+                border: '3px solid',
+                borderColor: 'background.surface',
+            }}
+            >
+            Vice President
+            </Chip>
+            <Typography level="title-lg">Oliver Chu</Typography>
+            <Typography level="body-sm" sx={{ maxWidth: '24ch' }}>
+            Data Science and Business at the University of California, Irvine
+            </Typography>
+            <Box
+            sx={{
+                display: 'flex',
+                gap: 2,
+                mt: 2,
+                '& > button': { borderRadius: '2rem' },
+            }}
+            >
+            <IconButton size="sm" variant="plain" color="neutral">
+                <SvgIcon>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                >
+                    <a href="https://www.linkedin.com/in/oliver-chu/" target="_blank" rel="noopener noreferrer">
+                        <path
+                            fill="currentColor"
+                            d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
+                    </a>
+                </svg>
+                </SvgIcon>
+            </IconButton>
+            </Box>
+        </CardContent>
+        <CardOverflow sx={{ bgcolor: 'background.level1' }}>
+            <CardActions buttonFlex="1">
+            <ButtonGroup variant="outlined" sx={{ bgcolor: 'background.surface' }}>
+                <Button onClick={toggleOliverEmail}>Message</Button>
+            </ButtonGroup>
+            {showOliver && <div>ojchu@uci.edu</div>}
             </CardActions>
         </CardOverflow>
         </Card>
