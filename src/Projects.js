3341e8d1876a1e3e6ca22dbe054af809d3a08d48
shakeelriyaj <shakeel.riyaj@gmail.com>
2023-09-20 00:36:12 -0700

    clean

--- src/Projects.js
+++ src/Projects.js
@@ -3,78 +3,9 @@ import graph from "./assets/Graph.png";
 import equation from "./assets/CovEq.png";
 import { Box } from "@mui/joy";
 import React from "react";
-import axios from "axios";
-import { useEffect, useState } from "react";
 
 const Projects = () => {
-    const [timer, setTimer] = useState(0);
-    const [apiGo, setApiGo] = useState(false);
-    const [AAdata, setAAData] = useState(null); // State to store the API data
-    const [Cdata, setCData] = useState(null); // State to store the API data
 
-    const checkTime = () => {
-        const currentDate = new Date();
-        const startTime = new Date();
-        startTime.setHours(6, 30, 0); // Set the start time to 6:30 AM
-        const endTime = new Date();
-        endTime.setHours(13, 0, 0); // Set the end time to 1:00 PM
-      
-        if (currentDate >= startTime && currentDate <= endTime) {
-            setApiGo(!apiGo)
-            console.log(apiGo)
-        } else {
-        }
-      };
-
-    const fetchData = () => {
-      // Fetch data for AA
-      axios
-        .get("https://api.twelvedata.com/time_series?apikey=e4e57fcf63f941b694fda658873d3445&interval=1min&symbol=AA")
-        .then((response) => {
-            const aaResponse = response.data.values[0].close;
-            setAAData(aaResponse)
-        })
-        .catch((error) => {
-          console.error("Error fetching AA data:", error);
-        });
-  
-      // Fetch data for C
-      axios
-        .get("https://api.twelvedata.com/time_series?apikey=e4e57fcf63f941b694fda658873d3445&interval=1min&symbol=C")
-        .then((response) => {
-            const CResponse = response.data.values[0].close;
-            setCData(CResponse)
-        })
-        .catch((error) => {
-          console.error("Error fetching C data:", error);
-        });
-    };
-  
-    useEffect(() => {
-      const intervalId = setInterval(() => {
-        setTimer((prevTimer) => prevTimer + 1);
-      }, 1000);
-
-      fetchData();
-
-      // Clean up timer and interval when the component unmounts
-      return () => {
-        clearInterval(intervalId);
-      };
-
-    }, []);
-  
-    useEffect(() => {
-      // Check if timer has reached 60 seconds (1 minute)
-      if (timer === 60 & apiGo) {
-        // Reset timer
-        setTimer(0);
-  
-        // Fetch data when the timer reaches 1 minute
-        fetchData();
-      }
-    }, [timer]);
-  
   return (
     <div className="templateContainer">
     <div className="mainHeader">Pairs Trading using Statistical Arbitrage</div>
@@ -82,12 +13,6 @@ const Projects = () => {
     The main languages of choice are Python and C++, which allow for readability among platforms and rapid compiling and executing time.
     This project aims to bring trading experience and developer knowledge to our members, while promoting a commonly used strategy in the quantititave finance world.</div>
       <div className="imageContainer">
-        <div>
-            <div className="AAstockTicker"> Alcoa Corp current price: ${AAdata} </div>
-            <br/>
-            <div className="CstockTicker">Citi Bank current price: ${Cdata} </div>
-        </div>
-        {checkTime()}
         <Box
           component="img"
           sx={{
