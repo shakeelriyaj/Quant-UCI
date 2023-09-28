dc8863d43bb3f7db887aef756624a638cc8e0278
shakeelriyaj <shakeel.riyaj@gmail.com>
2023-09-28 16:57:22 -0700

    update

--- src/frontpage.js
+++ src/frontpage.js
@@ -1,14 +1,14 @@
-import React from "react";
-import IntroVideo from "./assets/fintech.mp4";
-import "./styles/introduction.css"; 
 import { Box } from "@mui/joy";
-import MemberCard from "./MemberCard"
-import Projects from "./Projects"
-import Events from "./Events"
-import Apply from "./Apply"
+import React from "react";
+import Apply from "./Apply";
+import Events from "./Events";
+import MemberCard from "./MemberCard";
+import Projects from "./Projects";
+import IntroVideo from "./assets/fintech.mp4";
+import "./styles/introduction.css";
 
-const MainPage = ({selectedTab}) => {
-  if (selectedTab === 0){
+const MainPage = ({ selectedTab }) => {
+  if (selectedTab === 0) {
     return (
       <div>
         <Introduction />
@@ -16,38 +16,34 @@ const MainPage = ({selectedTab}) => {
         <MissionStatement />
       </div>
     );
-  };
-  if (selectedTab === 1){
-    return (
-      <Projects/>
-      );
-  };
-  if (selectedTab === 2){
-    return (
-      <Events/>
-      );
-  };
-  if (selectedTab === 3){
+  }
+  if (selectedTab === 1) {
+    return <Projects />;
+  }
+  if (selectedTab === 2) {
+    return <Events />;
+  }
+  if (selectedTab === 3) {
     return (
       <div>
-        <MembersCard/>
+        <MembersCard />
       </div>
-      );
-  };
-  if (selectedTab === 4){
+    );
+  }
+  if (selectedTab === 4) {
     return (
       <div>
-        <Apply/>
+        <Apply />
       </div>
-      );
-  };
-}
+    );
+  }
+};
 
 const Introduction = () => {
   return (
     <Box className="CenteredMainVideo">
       <Box className="video-container">
-      <Box className="HeaderFont">Welcome to Quant@UCI!</Box>
+        <Box className="HeaderFont">Welcome to Quant@UCI!</Box>
         <video width="1500vw" height="auto" autoPlay loop muted>
           <source src={IntroVideo} type="video/mp4" />
         </video>
@@ -60,16 +56,19 @@ const MissionStatement = () => {
   return (
     <>
       <div className="MissionBox">Mission Statement</div>
-      <div className="MissionWords">The purpose of this organization is to provide a space for students interested in financial
-technology to discuss topics and apply practical knowledge in areas pertaining to electronic
-trading or careers in finance. FinTech at UCI also serves to help engineering students develop
-professional networks with established companies and other students.</div>
+      <div className="MissionWords">
+        The purpose of this organization is to provide a space for students
+        interested in financial technology to discuss topics and apply practical
+        knowledge in areas pertaining to electronic trading or careers in
+        finance. Quant at UCI also serves to help engineering students develop
+        professional networks with established companies and other students.
+      </div>
     </>
   );
 };
 
 const MembersCard = () => {
-  return <MemberCard/>
-}
+  return <MemberCard />;
+};
 
 export default MainPage;
